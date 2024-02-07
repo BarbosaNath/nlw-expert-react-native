ecd1223a256528c472761f1eccbb5edc607de8db
Nath <playerxmatt@gmail.com>
2024-02-07 14:39:59 -0300

    feat: updated index component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
+import Header from "@/components/header";
 import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello World, React Native!</Text>
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" />
     </View>
   );
 }
-
