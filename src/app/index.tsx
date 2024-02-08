b707452e70eb61a6f81d4ee02a38b2accdaa5b9d
Nath <playerxmatt@gmail.com>
2024-02-08 16:59:16 -0300

    feat: added category buttons to index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,35 @@
+import { CategoryButton } from "@/components/category-button";
+import { CATEGORIES } from "@/utils/data/products";
 import Header from "@/components/header";
-import { View, Text } from "react-native";
+import { View, FlatList } from "react-native";
+import { useState } from "react";
 
 export default function Home() {
+  let [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuantityItems={5}/>
+      <Header title="Faça seu pedido" cartQuantityItems={5} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
