a03b33a2b3bcb32d39d744ef2200255373a3b25f
Nath <playerxmatt@gmail.com>
2024-02-09 19:55:18 -0300

    feat: added input to cart page

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
 import { View, Text, ScrollView } from "react-native";
 import { Product } from "@/components/product";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import { useCartStore } from "@/store/cart-store";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -18,24 +20,32 @@ export default function Cart() {
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
-      <ScrollView>
-        {cartStore.products.length > 0 ? (
+        <ScrollView>
           <View className="p-5 flex-1">
-            {cartStore.products.map((product) => (
-              <Product key={product.id} data={product} />
-            ))}
-          </View>
-        ) : (
-            <Text className="font-body text-slate-400 text-center my-8">
-              Seu carrinho está vazio.
-            </Text>
-          )}
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product key={product.id} data={product} />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio.
+              </Text>
+            )}
 
-        <View className="flex-row gap-2 items-center mt-5 mb-4">
-          <Text className="text-white text-xl font-subtitle">Total: {}</Text>
-          <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
-        </View>
-      </ScrollView>
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">
+                Total: {}
+              </Text>
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+          </View>
+        </ScrollView>
     </View>
   );
 }
