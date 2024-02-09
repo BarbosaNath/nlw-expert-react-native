2b3df34ef9f79b8525f724555656afc36d0cec9d
Nath <playerxmatt@gmail.com>
2024-02-08 21:39:56 -0300

    feat: added button component

---
+++ src/components/button.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+type ButtonProps = TouchableOpacityProps & {
+  children: ReactNode;
+};
+
+function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      className="h-12 bg-lime-400 rounded-md items-center justify-center flex-row"
+      activeOpacity={0.7}
+      {...rest}
+    >
+      {children}
+    </TouchableOpacity>
+  );
+}
