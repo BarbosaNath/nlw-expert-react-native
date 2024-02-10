ff434ac9f3e46a156966cb457f3e6233035611a6
Nath <playerxmatt@gmail.com>
2024-02-09 22:03:00 -0300

    feat: persistent storage added to cart

--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -1,5 +1,9 @@
-import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { ProductProps } from "@/utils/data/products";
+
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export type ProductCartProps = ProductProps & {
@@ -12,15 +16,23 @@ type StateProps = {
   remove: (productId: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
 
-  remove: (productId: string) =>
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    })),
-}));
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
